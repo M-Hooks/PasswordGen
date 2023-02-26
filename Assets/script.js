4acff37aa42ba59f2f7a88d318e9b006f180bb67
Meni Hooks <menihooks1@gmail.com>
2023-02-26 13:01:31 -0500

    added code in j.s regarding it's variables and functionality code for the button that still isn't working

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,62 @@
 // Assignment code here
+var Letters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+    Letters.sort()
+var CapLetters = toUpperCase().Letters
+var Numbers = [0,1,2,3,4,5,6,7,8,9]
+var S_Char = ["!","#","@","%","^","&","*","<",">",",","/","?"]
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var Length = prompt("How long would you like your password? (It must be at least 8 Characters and no bigger than 20 Characters");
+  while (Length < 8 || Length > 20 ){
+    prompt("Number is too low or too high. Pick a number between 8 and 20");
+  }
+    alert("You Password will have "+ Length + " Characters in it" )
+    // Confirm Creteria
+  var CLetters = confirm("Would you like lower case letters in your password?")
+  var CCapLetters = confirm("Would you like lower Capital letters in your password?")
+  var CNumbers = confirm("Would you like lower Numbers in your password?")
+  var CS_Char = confirm("Would you like lower Special Characters in your password?")
+  while (!CLetters  && !CCapLetters && !CNumbers && !CS_Char){
+  prompt("Choose at least ONE Creteria to generate a password");
+  return; 
+}
+
+var CharacterSet = [];
+if (CLetters){
+  CharacterSet += Letters
+}
+if (CCapLetters){
+  CharacterSet += CapLetters
+}
+if (CNumbers){
+  CharacterSet += Numbers
+}
+if (CS_Char){
+  CharacterSet += S_Char
+}
+
+var passwordText = document.querySelector("#password");
+
+function generatePassword(){
+  for( var i = 0; i< writePassword().Length, i++){
+    var randomChar = (math.random()*)
+  } 
+
+}
+
+var generateBtn = document.querySelector("#generate");
+
+  
 
   passwordText.value = password;
+  var password
 
 }
 
